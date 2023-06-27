d30f8348dbd56f7f57da8fd3ce9a40da32ee42cd
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-06-27 18:33:52 -0500

    Update install-refind.sh

--- install-refind.sh
+++ install-refind.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "enter sudo password"
-sudo echo "launching nix-shell with packages for refind to install, type \"refind-install\" once you are in the shell.
+sudo echo "launching nix-shell with packages for refind to install, type \"refind-install\" once you are in the shell."
 sudo nix-shell -p refind efibootmgr
