19c84f0658a84f93705b858ec1249a3bc7c966ec
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-06-29 13:54:55 -0500

    Update tmpfiles.nix

--- tmpfiles.nix
+++ tmpfiles.nix
@@ -1,9 +1,9 @@
 { config, pkgs, lib, ... }:
 
   systemd.tmpfiles.rules = [
-    "L /sw - - - - /run/current-system/sw "
+    "L /sw - - - - /run/current-system/sw"
     "L /nix/nix-profile - - - - /nix/var/nix/profiles/default"
-    "L /bin/bash - - - - /run/current-system/sw/bin/bash "
+    "L /bin/bash - - - - /run/current-system/sw/bin/bash"
   ];
 
 }
