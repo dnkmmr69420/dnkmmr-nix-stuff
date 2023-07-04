c7ff9cc0477217a5321f3dddfb38ba4668722891
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-07-03 22:14:16 -0500

    Create env-var.nix

---
+++ env-var.nix
@@ -0,0 +1,8 @@
+{ config, pkgs, ... }:
+
+{
+  environment.sessionVariables = {
+    PATH = "$HOME/.local/bin:$HOME/bin:/usr/local/bin:/usr/bin:/bin:$PATH";
+    XDG_DATA_DIRS = "$HOME/.local/share:/usr/local/share:/usr/share:$XDG_DATA_DIRS"
+  };
+}
