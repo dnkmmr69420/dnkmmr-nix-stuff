733faf629bc91df3cc69d8f3a22b4ab34622c1e2
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-07-04 09:23:56 -0500

    Update env-var.nix

--- env-var.nix
+++ env-var.nix
@@ -3,6 +3,5 @@
 {
   environment.sessionVariables = {
     PATH = "$HOME/.local/bin:$HOME/bin:/usr/local/bin:/usr/bin:/bin:$PATH";
-    XDG_DATA_DIRS = "$HOME/.local/share:/usr/local/share:/usr/share:$XDG_DATA_DIRS";
   };
 }
